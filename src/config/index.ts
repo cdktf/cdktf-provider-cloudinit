962e7a96d573d2c814b4538bbae06192eda976ac
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-22 05:50:37 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@6a21578

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/cloudinit/2.3.5/docs/resources/config
 // generated from terraform resource schema
 
